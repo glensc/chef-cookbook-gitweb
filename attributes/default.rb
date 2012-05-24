42d3bda000e824859d0b5ff04b2a47500ea36dbb
nickola <nickola@nickola.ru>
2012-05-25 00:29:27 +0400

    first commit

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,19 @@
+# Base settings
 default.gitweb[:owner] = "git"
 default.gitweb[:group] = "www-data"
 default.gitweb[:config] = "/etc/gitweb.conf"
 default.gitweb[:project_root] = "/home/git/repositories"
 default.gitweb[:base_url_list] = []
-# Apache
+
+# Apache settings
 default.gitweb[:server_port] = 80
 default.gitweb[:server_name] = "git.#{node[:domain]}"
 default.gitweb[:server_aliases] = nil
 default.gitweb[:document_root] = "/usr/share/gitweb"
-# Nginx
+
+# Nginx settings
 default.gitweb[:nginx_proxy] = false
 default.gitweb[:nginx_port] = 80
-# Theme
+
+# Theme settings
 default.gitweb[:theme] = "https://github.com/kogakure/gitweb-theme.git"
